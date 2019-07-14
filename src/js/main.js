f23e90f47cae9ae5d90359e6eb5aa39938e1d3c8
GodoFredo <godofredo207@gmail.com>
2019-07-14 00:46:05 -0500

    add zoom image .. how medium

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,17 @@ import 'lazysizes'
 // Impost
 import { loadScript } from './app/app.load-style-script'
 
+//
+/* Iframe SRC video */
+const iframeVideo = [
+  'iframe[src*="player.vimeo.com"]',
+  'iframe[src*="dailymotion.com"]',
+  'iframe[src*="youtube.com"]',
+  'iframe[src*="youtube-nocookie.com"]',
+  'iframe[src*="vid.me"]',
+  'iframe[src*="kickstarter.com"][src*="video.html"]'
+]
+
 const setup = () => {
   const qs = document.querySelector.bind(document)
   const qsa = document.querySelectorAll.bind(document)
@@ -19,6 +30,44 @@ const setup = () => {
     // -----------------------------------------------------------------------------
     if (qsa('.article-inner pre').length) {
       loadScript(`${siteUrl}/assets/scripts/prismjs.js`)
+    }
+
+    // gallery
+    // -----------------------------------------------------------------------------
+    qsa('.kg-gallery-image > img').forEach(item => {
+      const container = item.closest('.kg-gallery-image')
+      const width = item.attributes.width.value
+      const height = item.attributes.height.value
+      const ratio = width / height
+      container.style.flex = ratio + ' 1 0%'
+    })
+
+    // add class for zoom img => medium zoom Image
+    qsa('.article-inner img').forEach(el => !el.closest('a') && el.classList.add('rimay-zoom'))
+
+    // medium-zoom
+    const rimayZoom = qsa('.rimay-zoom')
+    if (rimayZoom.length) {
+      loadScript('https://unpkg.com/medium-zoom@1.0.4/dist/medium-zoom.min.js', () => {
+        // https://github.com/francoischalifour/medium-zoom#api
+        window.mediumZoom('.rimay-zoom', {
+          margin: 20,
+          background: 'hsla(0,0%,100%,.85)'
+        })
+      })
+    }
+
+    // Video Responsive
+    // -----------------------------------------------------------------------------
+    const allIframeVideo = qsa(iframeVideo.join(','))
+
+    if (allIframeVideo.length) {
+      allIframeVideo.forEach(el => {
+        const parentForVideo = document.createElement('div')
+        parentForVideo.className = 'video-responsive'
+        el.parentNode.insertBefore(parentForVideo, el)
+        parentForVideo.appendChild(el)
+      })
     }
   }
 }
