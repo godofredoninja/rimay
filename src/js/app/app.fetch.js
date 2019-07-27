11b0fd82d9fe03e1d9d99ba3200d486f1337b392
GodoFredo <godofredo207@gmail.com>
2019-07-27 14:03:50 -0500

    merge

--- src/js/app/app.fetch.js
+++
@@ -1,6 +0,0 @@
-export default async (url) => {
-  const rsp = await fetch(url)
-  const data = await rsp.json()
-
-  return data
-}
--- src/js/prismjs.js
+++
@@ -1,3 +0,0 @@
-import 'prismjs'
-import 'prismjs/plugins/autoloader/prism-autoloader'
-Prism.plugins.autoloader.languages_path = `${siteUrl}/assets/scripts/components/` // eslint-disable-line
